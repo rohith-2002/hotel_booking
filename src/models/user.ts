bb28897ef3a9b0fc333dfd3247e4c193b61aa4c9
Rohith Gundu <Rohith.Gundu@godigit.com>
2024-08-05 17:41:28 +0530

    created the user model

---
+++ src/models/user.ts
@@ -0,0 +1,41 @@
+class user{
+    id:number;
+    username:string;
+    phone:number;
+    email:string;
+
+    constructor(id:number,username:string,phone:number,email:string){
+        this.id=id;
+        this.username=username;
+        this.phone=phone;
+        this.email=email;
+    }
+
+    getUserId():number{
+        return this.id;
+    }
+    getUserName():string{
+        return this.username;
+    }
+    getUserPhone():number{
+        return this.phone;
+    }
+    getUserEmail():string{
+        return this.email;
+    }
+    setUserId(id:number){
+        this.id=id;
+    }
+    setUserName(username:string){
+        this.username=username;
+    }
+    setUserPhone(phone:number){
+        this.phone=phone;
+    }
+    setUserEmail(email:string){
+        this.email=email;
+    }
+    toString():string{
+        return this.id+" "+this.username+" "+this.phone+" "+this.email;
+    }
+}
