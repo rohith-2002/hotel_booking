043a259d5adc314ab4979cc8a64ca155a5d7d212
Rohith Gundu <Rohith.Gundu@godigit.com>
2024-08-06 14:19:18 +0530

    implemented the booking

--- src/services/bookingservices.ts
+++ src/services/bookingservices.ts
@@ -1,3 +1,42 @@
-import { hotel } from "../models/hotel";
 import { booking } from "../models/booking";
-import { bookingStatus } from "../models/bookingStatus";
+
+
+export default class bookingservices{
+
+    public booking: booking[]=[];
+    public addBooking(booking: booking): void {
+        this.booking.push(booking);
+    }
+    public getBookingbyId(id: number): undefined | booking {
+        console.log( this.booking.find(booking => booking.bookingId === id));
+        return this.booking.find(booking => booking.bookingId === id);
+    }
+    public getBookings(): booking[] {
+        return this.booking;
+    }
+    public removeBooking(id: number): void {
+        const index = this.booking.findIndex(booking => booking.bookingId === id);
+        if(index!==-1){
+            this.booking.splice(index,1);
+        }
+        console.log(this.booking);
+    }
+    public updateBooking(newbooking: booking,id :number): void {
+       const index = this.booking.findIndex(booking => booking.bookingId === id);
+       if(index!==-1){
+         this.booking[index]=newbooking;
+       }
+       console.log("booking ubdated");
+    }
+
+    // public getBookingByUserId(id: number): booking[] {
+    //     const userService = new UserService();
+    //     const user = userService.getUserbyId(id);
+    //     return this.booking.filter(booking => booking.userId === user);
+    // }
+    // public getBookingByHotelId(id: number): booking[] {
+    //     const hotelService = new HotelService();
+    //     const hotel = hotelService.getHotelbyId(id);
+    //     return this.booking.filter(booking => booking.hotel === hotel);
+    // }
+}
